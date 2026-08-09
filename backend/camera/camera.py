2d23c46047291a1900cbfa9c7fd601911eda4b0e
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-08-09 13:22:01 +0500

    fps added in camera module

--- backend/camera/camera.py
+++ backend/camera/camera.py
@@ -1,11 +1,15 @@
 import cv2
 import time
 
-def open_camera(camera_index=0):
+def open_camera(camera_index=0, width=1280, height=720, fps=30):
     camera = cv2.VideoCapture(camera_index)
 
     if not camera.isOpened():
         raise RuntimeError("Could not Open Camera")
+
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    camera.set(cv2.CAP_PROP_FPS, fps)
 
     return camera
 
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 from flask import Flask
 from flask_cors import CORS
 from camera.camera import open_camera, read_frame, release_camera
@@ -9,21 +10,43 @@ def create_app():
     CORS(app,
     supports_credentials=True
     )
-    camera = open_camera()
-    while True:
-        frame = read_frame(camera)
-        cv2.imshow("Persentria", frame)
-        
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    release_camera(camera)
-    cv2.destroyAllWindows()
+    camera = open_camera(
+        camera_index=0,
+        width=1280,
+        height=720,
+        fps=30
+    )
+    previous_time = time.time()
+    try:
 
+        while True:
+            frame = read_frame(camera)
+            current_time = time.time()
+            elapsed_time = current_time - previous_time
+            fps = 1 / elapsed_time if elapsed_time > 0 else 0
+            
+            previous_time = current_time
+            
+            cv2.putText(
+                frame,
+                f"FPS: {fps:.1f}",
+                (20, 40),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2
+            )
+            cv2.imshow("Persentria", frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+       
+    except RuntimeError as error:
+        print(f"Camera error: {error}")
 
-
-
-
-    
+    finally:
+        release_camera(camera)
+        cv2.destroyAllWindows()
     return app
 
 
