275369359d07821f39ebf4c046a13443c7c7b556
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-08-18 14:29:33 +0500

    talking.py

--- backend/analysis/talking.py
+++ backend/analysis/talking.py
@@ -7,6 +7,8 @@ LOWER_LIP = 14
 LEFT_MOUTH = 61
 RIGHT_MOUTH = 291
 
+talking_states = {}
+
 def calculate_distance(point1,point2):
     return math.sqrt((point1.x - point2.x) ** 2 +
     (point1.y - point2.y) ** 2
@@ -27,3 +29,53 @@ def calculate_mouth_ratio(landmarks):
         return 0
 
     return mouth_height / mouth_width
+
+def detect_talking(
+    face_landmarks,
+    person_id,
+    thresold = 0.08
+):
+    mouth_ratio = calculate_mouth_ratio(
+        face_landmarks
+    )
+
+    current_time = time.monotonic()
+
+    if person_id not in talking_states:
+        talking_states[person_id] = {
+            "previous_ratio": mouth_ratio,
+            "movement_count":0,
+            "last_movement":current_time,
+            "talking": False
+        }
+    
+    state = talking_states[person_id]
+
+    difference = abs(
+        mouth_ratio - state["previous_ratio"]
+    )
+
+    if difference > thresold:
+        state["movement_count"] += 1
+        state["last_movement"] = current_time
+
+    state["previous_ratio"] = mouth_ratio
+
+    time_since_movement = (
+        current_time - state["last_movement"]
+    )
+
+    if (
+        state["movement_count"] >= 3
+        and time_since_movement < 0.8
+    ):
+        state["talking"] = True
+    else:
+        state["talking"] = False
+
+    return {
+        "talking": state["talking"],
+        "mouth_ratio": mouth_ratio,
+        "movement_count": state["movement_count"]
+    }
+        
